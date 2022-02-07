e0ec24e23e36aa4dbf4b57b7e0e95a47c89ed908
root <root@localhost>
2022-02-07 09:29:46 +0530

    auto publish 1644206385835

---
+++ src/main.py
@@ -0,0 +1,26 @@
+i = 1
+sum = 0
+while i <= 10:
+   sum = sum + i
+   i = i + 1
+print (sum)
+
+
+
+# print(1+2+3+4+5+6+7+8+9+10) another way to understand the code
+
+# subdivision of the above code
+
+sum = sum + i
+ 0     0    1
+ 1     1    2
+ 3     3    3
+ 6     6    4
+ 10    10   5
+ 15    15   6
+ 21    21   7
+ 28    28   8
+ 36    36   9
+ 45    45   10
+ 55    loop end enjoy
+  
