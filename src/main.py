1e7e92e68d45e101b535f7a75b280faf4d46bde1
root <root@localhost>
2022-02-07 10:17:40 +0530

    auto publish 1644209260544

--- src/main.py
+++ src/main.py
@@ -1,26 +1,26 @@
 i = 1
 sum = 0
 while i <= 10:
-   sum = sum + i
-   i = i + 1
-print (sum)
+   if i%2 == 0:
+      sum = sum + i
+   i = i+1
 
+print(sum)
 
+# breakdown of the above code
 
-# print(1+2+3+4+5+6+7+8+9+10) another way to understand the code
-
-# subdivision of the above code
-
-sum = sum + i
- 0     0    1
- 1     1    2
- 3     3    3
- 6     6    4
- 10    10   5
- 15    15   6
- 21    21   7
- 28    28   8
- 36    36   9
- 45    45   10
- 55    loop end enjoy
-  
+'''sum = sum + i
+    0  =  0  + 1 --> here if condition false 
+    0  =  0  + 2 --> here if condition true sum become 2
+    2  =  2  + 3 --> here if condition false
+    2  =  2  + 4 --> here if condition true sum become 6
+    6  =  6  + 5 --> here if condition false
+    6  =  6  + 6 --> here if condition true sum become 12
+    12 =  12 + 7 --> here if condition false
+    12 =  12 + 8 --> here if condition true sum become 20
+    20 =  20 + 9 --> here if condition false
+    20 =  20 + 10--> here if condition true sum become 30
+    30 ---------->''' # this will be the final output of the above code
+   
+''''best way to understand any code and to accelerate your logical building  
+skill is to sub divide the code'''
